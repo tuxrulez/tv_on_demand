75033a0f7cda987efc6571b03a74d72f756d083f
Heigler <heigler.rocha@megamidia.com.br>
2010-09-23 09:39:14 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+#-*- coding: utf-8 -*-
+from distutils.core import setup
+
+VERSION = '0.1'
+
+setup = setup(
+    name='megamidia-tv_on_demand',
+    version=VERSION,
+    description=u'Módulo de TV sob demanda',
+    author='Heigler Rocha',
+    author_email='heigler.rocha@megamidia.com.br',
+    platforms=['any'],
+    packages=[
+        'tv_on_demand',
+    ]
+)
+
