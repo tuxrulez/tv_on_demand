99127cea0e369a2b1ded1e5df96ae776ea6cd062
Heigler <heigler.rocha@megamidia.com.br>
2011-02-17 10:48:45 -0200

    fixed

--- tv_on_demand/views.py
+++ tv_on_demand/views.py
@@ -164,7 +164,7 @@ def live(request):
     
     #context = {'live_filename': live_reader.file_name or 'nofile'}
     #response = direct_to_template(request, template='tv_on_demand/live.html', extra_context=context)
-    os.system('cvlc mms://10.0.100.14:8080 --fullscreen')  
+    #os.system('cvlc mms://10.0.100.14:8080 --fullscreen')  
     
     return HttpResponse('ok')
     
