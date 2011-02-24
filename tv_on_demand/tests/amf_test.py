69d0c7540e01252b1c5b5ef37a5190f367f34285
Heigler <heigler.rocha@megamidia.com.br>
2011-02-24 15:12:58 -0300

    flash support added

---
+++ tv_on_demand/tests/amf_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pyamf.remoting.client import RemotingService as RE
+
+url = 'http://localhost:8000/admin/tv_on_demand/amf-structure/'
+gw = RE(url)
+service = gw.getService('structure')
+
+print service.rows({'structure_id': 1})
+print service.rows({'structure_id': 1, 'parent_id': 2})
+print service.verify_user({'row_id': 1})
+print service.login({'row_id': 1, 'username': 'admin', 'password': 'admin123'})
--- tv_on_demand/views.py
+++ tv_on_demand/views.py
@@ -7,10 +7,11 @@ from django.views.generic.simple import direct_to_template
 from django.contrib.auth.decorators import permission_required
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseForbidden
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, Http404
 from django.utils import simplejson
 from django.core.urlresolvers import reverse
 from django.template import TemplateDoesNotExist
+from pyamf.remoting.gateway.django import DjangoGateway
 from tv_on_demand.forms import StructureForm, StructureRowForm
 from tv_on_demand.models import Structure, StructureRow
 from tv_on_demand.helpers import LiveFileReader
@@ -82,50 +83,7 @@ def structurerow_ajax_delete(request, object_id):
     instance = get_object_or_404(StructureRow, pk=object_id)
     instance.delete()
     
-    return HttpResponse('deleted')
-    
-def generic_main(request, structure_id, template='tv_on_demand/main.html'):
-    structure = get_object_or_404(Structure, pk=structure_id)
-    root_rows = StructureRow.objects.filter(parent=None, structure=structure).order_by('order')
-    context = {'structure': structure,
-               'root_rows': root_rows}
-    
-    response = direct_to_template(request, template=template, extra_context=context)
-    return response    
-    
-    
-def main(request, structure_id):
-    return generic_main(request, structure_id)
-    
-
-def pure_main(request, structure_id):
-    return generic_main(request, structure_id, template='tv_on_demand/pure_main.html')
-    
-
-def children_of(request, father_id):
-    father_row = get_object_or_404(StructureRow, pk=father_id)
-    allowed_users = father_row.users.all()
-    
-    # TODO: fazer verificação para os filhos
-    
-    if allowed_users:
-        if not request.user.is_authenticated():
-            return HttpResponse('not_allowed')
-            
-        if not request.user in allowed_users:
-            return HttpResponse('not_allowed')
-    
-    rows = father_row.children.all()
-    context = {'rows': rows, 'father_row': father_row}
-    
-    client_tmpl = 'tv_on_demand/%s/rows.html' % father_row.structure.skin.slug
-
-    try:
-        response = direct_to_template(request, template=client_tmpl, extra_context=context)
-    except TemplateDoesNotExist:
-        response = direct_to_template(request, template='tv_on_demand/rows.html', extra_context=context)
-        
-    return response
+    return HttpResponse('deleted')    
     
     
 def serve_video(request, row_id, video_id):
@@ -135,67 +93,95 @@ def serve_video(request, row_id, video_id):
     
     context = {'rows': rows, 'selected_row': selected_row, 'father_row': cur_row}
     response = direct_to_template(request, template='tv_on_demand/video_list.html', extra_context=context)
-    return response
-    
-    
-def live_media(request, filename):
-    # serve um vídeo direto do diretório importer/tv_on_demand/live
-    # baseado no nome do arquivo via GET
-    file_path = os.path.join(settings.MODPATH, 'importer', 'tv_on_demand', 'live', filename)
-  
-    if not os.path.exists(file_path):
-        return HttpResponseForbidden('file does not exist')
-        
-    file_ext = filename.split('.')[-2]
-    if file_ext == 'avi':
-        mime_type = 'video/x-msvideo'
-    else:
-        mime_type = 'video/mpeg'
-    
-    file_media = open(file_path, 'r').read()
-    
-    response = HttpResponse(file_media, mimetype=mime_type)
-    return response
-    
+    return response    
+
     
 def live(request):
-    #live_reader = LiveFileReader()
-    #live_reader.select_file()
-    
-    #context = {'live_filename': live_reader.file_name or 'nofile'}
-    #response = direct_to_template(request, template='tv_on_demand/live.html', extra_context=context)
-    #os.system('cvlc mms://10.0.100.14:8080 --fullscreen')  
-    
     return HttpResponse('ok')
     
-    
-def do_login(request, row_id):
-    row = get_object_or_404(StructureRow, pk=row_id)    
-    
-    if request.method == 'POST':
-        username = request.POST.get('username', '')
-        password = request.POST.get('password', '')
-        
-        user = authenticate(username=username, password=password)
-        
-        if user != None:
-            if user.is_active and user in row.users.all():                
-                login(request, user)
-                response = HttpResponse('login_true')
-            else:
-                response = HttpResponse('login_false')
-                
-        else:
-            response = HttpResponse('login_false')
-            
-    else:
-        response = direct_to_template(request, template='tv_on_demand/login.html',
-                                      extra_context={'row_url': reverse('tod_login', args=[row_id])})
-        
-    return response
-    
-    
+   
 def do_logout(request):
     logout(request)
     return HttpResponse('ok')
+    
+
+def amf_rows(request, amf_data):
+    try:
+        structure_id = amf_data['structure_id']
+        structure = Structure.objects.get(pk=structure_id)
+    except KeyError:
+        return ''
+    except Structure.DoesNotExist:
+        return ''
+        
+    parent_id = amf_data.get('parent_id', '')
+    
+    if not parent_id:
+        rows = StructureRow.objects.filter(parent=None, structure=structure).order_by('order')
+    else:
+        try:
+            parent = StructureRow.objects.get(pk=parent_id)
+        except StructureRow.DoesNotExist:
+            return ''
+        rows = StructureRow.objects.filter(parent=parent, structure=structure).order_by('order')        
+        
+    amf_output = []
+    for row in rows:
+        local_data = {'id': row.pk, 'title': row.title, 'description': row.label, 'type': row.mediafile.media_type,
+                      'file': row.mediafile.path and row.mediafile.path.url or '', 'structure_id': row.structure.id,
+                      'restricted': row.users.all() and True or False,
+                      'video_image': row.mediafile.video_image and row.mediafile.video_image.url or ''}
+        if row.mediafile.media_type == 'video':
+            local_data['video_play_url'] = reverse('tod_serve_video', args=[row.id, row.mediafile.pk])
+                      
+        amf_output.append(local_data)
+    
+    return amf_output
+    
+
+def amf_verify_user(request, amf_data):  
+    try:
+        requested_row = StructureRow.objects.get(pk=amf_data['row_id'])
+    except KeyError:
+        return False
+    except StructureRow.DoesNotExist:
+        return False
+        
+    if not request.user in requested_row.users.all():
+        return False
+        
+    return True
+
+
+def amf_login(request, amf_data):
+    try:
+        row = StructureRow.objects.get(pk=amf_data['row_id'])
+    except KeyError:
+        return False
+    except StructureRow.DoesNotExist:
+        return False
+        
+    username = amf_data.get('username', '')
+    password = amf_data.get('password', '')
+        
+    user = authenticate(username=username, password=password)
+        
+    if user != None:
+        if user.is_active and user in row.users.all():                
+            login(request, user)
+            return True
+        else:
+            return False
+                
+    return False
+
+    
+amf_services = {
+    'structure.rows': amf_rows,
+    'structure.verify_user': amf_verify_user,
+    'structure.login': amf_login,
+}
+
+amf_structure = DjangoGateway(amf_services)
+
   
