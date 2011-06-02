2c4db33b89b8449ccb510104310797de66658fda
Heigler <heigler.rocha@megamidia.com.br>
2011-06-02 11:08:20 -0300

    fixed

--- tv_on_demand/migrations/0001_initial.py
+++ tv_on_demand/migrations/0001_initial.py
@@ -13,19 +13,15 @@ class Migration(SchemaMigration):
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('title', self.gf('django.db.models.fields.CharField')(max_length=45)),
             ('slug', self.gf('django.db.models.fields.SlugField')(max_length=45, db_index=True)),
-            ('css_style', self.gf('django.db.models.fields.files.FileField')(max_length=150)),
-            ('external_id', self.gf('django.db.models.fields.PositiveIntegerField')(null=True, blank=True)),
         ))
         db.send_create_signal('tv_on_demand', ['Skin'])
 
         # Adding model 'Structure'
         db.create_table('tv_on_demand_structure', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('content_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'], null=True, blank=True)),
-            ('object_id', self.gf('django.db.models.fields.PositiveSmallIntegerField')(null=True, blank=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
             ('skin', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['tv_on_demand.Skin'])),
-            ('external_id', self.gf('django.db.models.fields.PositiveIntegerField')(null=True, blank=True)),
+            ('mediadatabase', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['mediafiles.MediaDatabase'])),
         ))
         db.send_create_signal('tv_on_demand', ['Structure'])
 
@@ -37,25 +33,9 @@ class Migration(SchemaMigration):
             ('title', self.gf('django.db.models.fields.CharField')(max_length=100)),
             ('label', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
             ('mediafile', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['mediafiles.MediaFile'], null=True, blank=True)),
-            ('entry', self.gf('django.db.models.fields.CharField')(max_length=45, null=True, blank=True)),
-            ('date_start', self.gf('django.db.models.fields.DateTimeField')()),
-            ('date_end', self.gf('django.db.models.fields.DateTimeField')()),
-            ('external_id', self.gf('django.db.models.fields.PositiveIntegerField')(null=True, blank=True)),
             ('order', self.gf('django.db.models.fields.PositiveIntegerField')()),
-            ('lft', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
-            ('rght', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
-            ('tree_id', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
-            ('level', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
         ))
         db.send_create_signal('tv_on_demand', ['StructureRow'])
-
-        # Adding M2M table for field users on 'StructureRow'
-        db.create_table('tv_on_demand_structurerow_users', (
-            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('structurerow', models.ForeignKey(orm['tv_on_demand.structurerow'], null=False)),
-            ('user', models.ForeignKey(orm['auth.user'], null=False))
-        ))
-        db.create_unique('tv_on_demand_structurerow_users', ['structurerow_id', 'user_id'])
 
 
     def backwards(self, orm):
@@ -69,103 +49,56 @@ class Migration(SchemaMigration):
         # Deleting model 'StructureRow'
         db.delete_table('tv_on_demand_structurerow')
 
-        # Removing M2M table for field users on 'StructureRow'
-        db.delete_table('tv_on_demand_structurerow_users')
-
 
     models = {
-        'auth.group': {
-            'Meta': {'object_name': 'Group'},
+        'mediafiles.genre': {
+            'Meta': {'object_name': 'Genre'},
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '45'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '45', 'db_index': 'True'})
         },
-        'auth.permission': {
-            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
-            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+        'mediafiles.mediadatabase': {
+            'Meta': {'object_name': 'MediaDatabase'},
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        },
-        'auth.user': {
-            'Meta': {'object_name': 'User'},
-            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
-            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
-            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
-        },
-        'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '255', 'db_index': 'True'}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '30'})
         },
         'mediafiles.mediafile': {
             'Meta': {'object_name': 'MediaFile'},
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']", 'null': 'True', 'blank': 'True'}),
             'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'external_id': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'duration': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'genre': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['mediafiles.Genre']", 'null': 'True', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'label': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'media_database': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['mediafiles.MediaDatabase']"}),
             'media_type': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
-            'object_id': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
             'path': ('django.db.models.fields.files.FileField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
-            'question': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['quizzes.Question']", 'null': 'True', 'blank': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        },
-        'quizzes.question': {
-            'Meta': {'object_name': 'Question'},
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']", 'null': 'True', 'blank': 'True'}),
-            'external_id': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'object_id': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'video_image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
         },
         'tv_on_demand.skin': {
             'Meta': {'object_name': 'Skin'},
-            'css_style': ('django.db.models.fields.files.FileField', [], {'max_length': '150'}),
-            'external_id': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'slug': ('django.db.models.fields.SlugField', [], {'max_length': '45', 'db_index': 'True'}),
             'title': ('django.db.models.fields.CharField', [], {'max_length': '45'})
         },
         'tv_on_demand.structure': {
             'Meta': {'object_name': 'Structure'},
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']", 'null': 'True', 'blank': 'True'}),
-            'external_id': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'mediadatabase': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['mediafiles.MediaDatabase']"}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'object_id': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
             'skin': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tv_on_demand.Skin']"})
         },
         'tv_on_demand.structurerow': {
             'Meta': {'object_name': 'StructureRow'},
-            'date_end': ('django.db.models.fields.DateTimeField', [], {}),
-            'date_start': ('django.db.models.fields.DateTimeField', [], {}),
-            'entry': ('django.db.models.fields.CharField', [], {'max_length': '45', 'null': 'True', 'blank': 'True'}),
-            'external_id': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'label': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
-            'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
             'mediafile': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['mediafiles.MediaFile']", 'null': 'True', 'blank': 'True'}),
             'order': ('django.db.models.fields.PositiveIntegerField', [], {}),
             'parent': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'children'", 'null': 'True', 'to': "orm['tv_on_demand.StructureRow']"}),
-            'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
             'structure': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tv_on_demand.Structure']"}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'users': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['auth.User']", 'null': 'True', 'blank': 'True'})
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         }
     }
 
--- tv_on_demand/models.py
+++ tv_on_demand/models.py
@@ -1,30 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import os
-from mptt.models import MPTTModel
 from django.db import models
-from django.contrib.auth.models import Group, User
 from django.utils.translation import ugettext_lazy as _
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes import generic
-from django.core.exceptions import ValidationError
 from django.template.defaultfilters import slugify
-from django.conf import settings
-from sorl.thumbnail import get_thumbnail
-from mmutils.common import auto_serialize
 from mediafiles.models import MediaFile, MediaDatabase
 
-
-ENTRY_TYPES = (('feed', 'Feed'),)
 
 class Skin(models.Model):
     title = models.CharField(_('title'), max_length=45)
     slug = models.SlugField(max_length=45, editable=False)
-    external_id = models.PositiveIntegerField(_('external id'), null=True, blank=True)
-    accept_video = models.BooleanField()
-    accept_audio = models.BooleanField()
-    accept_entry = models.BooleanField()
-    accept_image = models.BooleanField()
     
     class Meta:
         verbose_name = _('skin')
@@ -33,99 +17,39 @@ class Skin(models.Model):
     def __unicode__(self):
         return self.title
         
-    def get_allowed_medias(self):
-        formated_val = '%s,%s,%s,%s' %(self.accept_video and 'video' or '', self.accept_audio and 'audio' or '',
-                                       self.accept_entry and 'entry' or '', self.accept_image and 'image' or '')
-        return formated_val
-        
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
         super(Skin, self).save(*args, **kwargs)
         
-    def logo_thumb(self):
-        logo_file = os.path.join(settings.MEDIA_ROOT, 'skins', self.slug, 'logo.jpg')
-        thumb = get_thumbnail(logo_file, '160x260', quality=99)                
-        return thumb
         
 
 class Structure(models.Model):
-    content_type = models.ForeignKey(ContentType, verbose_name=_('content type'),
-                                     null=True, blank=True)
-    object_id = models.PositiveSmallIntegerField(_('object id'), null=True, blank=True)    
     name = models.CharField(_('name'), max_length=100)
-    skin = models.ForeignKey(Skin, verbose_name=_('skin'))
-    external_id = models.PositiveIntegerField(_('external id'), null=True, blank=True)
-    
-    content_object = generic.GenericForeignKey(ct_field='content_type', fk_field='object_id')
-    mediadatabase = models.ForeignKey(MediaDatabase, verbose_name=_('media database'))
-    
+    skin = models.ForeignKey(Skin, verbose_name=_('skin'))    
+    mediadatabase = models.ForeignKey(MediaDatabase, verbose_name=_('media database'))    
     
     class Meta:
         verbose_name = _('structure')
         verbose_name_plural = _('structures')
         
     def __unicode__(self):
-        return self.name
-        
-    def serialize(self):
-        return auto_serialize(self)
-        
-    @models.permalink
-    def get_absolute_url(self):
-        return ('admin:tv_on_demand_structure_change', [self.pk])
+        return self.name        
         
         
-class StructureRow(MPTTModel):
+class StructureRow(models.Model):
     structure = models.ForeignKey(Structure, verbose_name=_('structure'))
     parent = models.ForeignKey('self', null=True, blank=True, related_name='children')
     title = models.CharField(_('title'), max_length=100)
     label = models.TextField(_('description'), null=True, blank=True)
     mediafile = models.ForeignKey(MediaFile, verbose_name=_('media'), null=True, blank=True)
-    entry = models.CharField(_('entry'), max_length=45, null=True, blank=True, choices=ENTRY_TYPES)
-    date_start = models.DateTimeField(_('date start'))
-    date_end = models.DateTimeField(_('date end'))
-    external_id = models.PositiveIntegerField(_('external id'), null=True, blank=True)
     order = models.PositiveIntegerField(_('order'))
-    groups = models.ManyToManyField(Group, verbose_name=_('allowed groups'), null=True, blank=True)
-    users = models.ManyToManyField(User, verbose_name=_('allowed_users'), null=True, blank=True)
     
     class Meta:
         verbose_name = _('row')
         verbose_name_plural = _('rows')
         
-    class MPTTMeta:
-        order_insertion_by=['order']
-        
     def __unicode__(self):
-        return self.title
-        
-    def br_datetime(self, field):
-        value = getattr(self, field)
-        return value.strftime('%d/%m/%Y %H:%M')
-        
-    def serialize(self):
-        return auto_serialize(self, datetime_format='%d/%m/%Y %H:%M') 
-        
-    def restricted(self):
-        #retorna se o objeto tem restrições de usuários ou não
-        return self.users.all() and True or False    
-    
-    
-# signal que remove usuários que já não pertecem mais à determinados grupos
-def structurerow_create_update(signal, instance, sender, created, **kwargs):
-    allowed_users = []
-    
-    for group in instance.groups.all():
-        for user_group in group.user_set.all():
-            allowed_users.append(user_group)    
-    
-    for user in instance.users.all():
-        if not user in allowed_users:
-            instance.users.remove(user)
-            
-            
-    user_rows = StructureRow.objects.filter(users=instance)
-        
-models.signals.post_save.connect(structurerow_create_update, sender=StructureRow)
+        return self.title     
 
-      
+    
+  
