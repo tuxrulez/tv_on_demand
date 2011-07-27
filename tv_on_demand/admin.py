fbd9d08f110f9b6afdfb1371724e8c284ba69473
Heigler <heigler.rocha@megamidia.com.br>
2011-07-27 10:37:23 -0300

    fixed

--- tv_on_demand/admin.py
+++ tv_on_demand/admin.py
@@ -3,7 +3,11 @@
 from django.contrib import admin
 from tv_on_demand.models import Skin, Structure, StructureRow
 
+class StructureAdmin(admin.ModelAdmin):
+    list_filter = ['chain', 'store']
+    
+
 admin.site.register(Skin)
-admin.site.register(Structure)
+admin.site.register(Structure, StructureAdmin)
 admin.site.register(StructureRow)
 
