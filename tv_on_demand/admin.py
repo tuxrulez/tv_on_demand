9d668b739f51e69342edd9afb3a359bf6bfdb0d9
Heigler <heigler.rocha@megamidia.com.br>
2011-08-04 16:27:26 -0300

    fixed

--- tv_on_demand/admin.py
+++ tv_on_demand/admin.py
@@ -5,9 +5,12 @@ from tv_on_demand.models import Skin, Structure, StructureRow
 
 class StructureAdmin(admin.ModelAdmin):
     list_filter = ['chain', 'store']
-    
+
+class StructureRowAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+
 
 admin.site.register(Skin)
 admin.site.register(Structure, StructureAdmin)
-admin.site.register(StructureRow)
+admin.site.register(StructureRow, StructureRowAdmin)
 
